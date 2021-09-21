a6b85a34a97fdfda120c37532949e53a89dfdaab
ivanandreich <iwanarmadillosolo1@gmail.com>
2021-09-21 22:31:31 +0300

    Used 'for' to get multiple responses

--- main.py
+++ main.py
@@ -4,22 +4,24 @@ import pathlib
 from pathlib import Path
 
 url = 'https://api.discogs.com/releases/'
-# for release_id in range(1, 10):
-#  res = requests.get(url + release_id)
+num = 100  # количество файлов
+dir_path = pathlib.Path.cwd()
 
+for release_id in range(1, num + 1):
+    res = requests.get(url + str(release_id))
+    path = Path(dir_path, 'venv', 'Scripts', 'files', str(release_id) + '.json')
+    with open(path, 'w') as write_file:
+        json.dump(res.json(), write_file)
 
-release_id = "200"
-res = requests.get(url + release_id)
+# release_id = "200"
+# res = requests.get(url + release_id)
 # path = Path("C", "Users", "63516", "Desktop", "DiscogsJSON", "File")
 # print(res.json())
-dir_path = pathlib.Path.cwd()
+
 # print(dir_path)
 
-path = Path(dir_path, 'venv', 'Scripts', 'files', 'File.json')
 
-# print(path)
 
-with open(path, 'w') as write_file:
-    json.dump(res.json(), write_file)
+
 
 
